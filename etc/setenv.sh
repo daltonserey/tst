8bb28046cc86597599903b516ddda5903c0b617a
Dalton Serey <daltonserey@gmail.com>
2016-08-23 11:39:28 -0300

    Fix setenv.sh to update only .profile and .bashrc

--- etc/setenv.sh
+++ etc/setenv.sh
@@ -2,12 +2,12 @@
 # coding: utf-8
 # (c) 2016 Dalton Serey, UFCG
 #
-# Configure bash environment for TST. 
+# Interactive script to configure bash environment for TST. 
 
 # constants
 TST_DIR=~/.tst
-USER_DOT_PROFILE=~/.profile
-USER_DOT_BASH_PROFILE=~/.bash_profile
+DOT_PROFILE=~/.profile
+DOT_BASHRC=~/.bashrc
 TST_PATH_INCLUDE=$TST_DIR/etc/tst.path.inc
 TST_PYTHONPATH_INCLUDE=$TST_DIR/etc/tst.pythonpath.inc
 
@@ -42,96 +42,68 @@ function print {
 }
 
 function update_dot_profile {
-    echo >> $USER_DOT_PROFILE
-    echo "# The next line sets up the PATH for tst" >> $USER_DOT_PROFILE
-    echo "source '$TST_PATH_INCLUDE'" >> $USER_DOT_PROFILE
-    echo >> $USER_DOT_PROFILE
-    echo "# The next line sets up the PYTHONPATH for tst" >> $USER_DOT_PROFILE
-    echo "source '$TST_PYTHONPATH_INCLUDE'" >> $USER_DOT_PROFILE
+    echo >> $DOT_PROFILE
+    echo "# The next line sets up the PATH for tst" >> $DOT_PROFILE
+    echo "source '$TST_PATH_INCLUDE'" >> $DOT_PROFILE
+    echo >> $DOT_PROFILE
+    echo "# The next line sets up the PYTHONPATH for tst" >> $DOT_PROFILE
+    echo "source '$TST_PYTHONPATH_INCLUDE'" >> $DOT_PROFILE
 }
 
 function update_dot_bash_profile {
-    echo >> $USER_DOT_BASH_PROFILE
-    echo "# Added by tst install script" >> $USER_DOT_BASH_PROFILE
-    echo "# Source both .profile and .bashrc for login shells" >> $USER_DOT_BASH_PROFILE
-    echo "if [ -f ~/.profile ]; then . ~/.profile; fi # Added by tst install script" >> $USER_DOT_BASH_PROFILE
-    echo "if [ -f ~/.bashrc ]; then . ~/.bashrc; fi # Added by tst install script" >> $USER_DOT_BASH_PROFILE
+    echo >> $DOT_BASHRC
+    echo "# Added by tst install script" >> $DOT_BASHRC
+    echo "# Source both .profile and .bashrc for login shells" >> $DOT_BASHRC
+    echo "if [ -f ~/.profile ]; then . ~/.profile; fi # Added by tst install script" >> $DOT_BASHRC
+    echo "if [ -f ~/.bashrc ]; then . ~/.bashrc; fi # Added by tst install script" >> $DOT_BASHRC
 }
 
-function create_dot_profile {
-    echo "# This file was created by the TST install procedure." >> $USER_DOT_PROFILE
-    echo "# However, it is not part of TST itself and you can" >> $USER_DOT_PROFILE
-    echo "# edit this file however you need." >> $USER_DOT_PROFILE
-    echo "# Keep the tst section below or move them to the" >> $USER_DOT_PROFILE
-    echo "# appropriate rc file, to make tst easier to use." >> $USER_DOT_PROFILE
-}
+function update_file {
+    FILE=$1
+    PATTERN_PATH="source.*tst.inc.*"
+    PATH_FILE=~/.tst/etc/tst.inc
+    TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+    LINE="source '$PATH_FILE' # $TIMESTAMP"
 
-function create_dot_bash_profile {
-    echo "# This file was created by the TST install script." >> $USER_DOT_BASH_PROFILE
-    echo "# However, it is not part of TST itself and you can" >> $USER_DOT_BASH_PROFILE
-    echo "# edit this file however you need." >> $USER_DOT_BASH_PROFILE
+    CHECK=$(grep -E "$PATTERN_PATH" $FILE 2> /dev/null)
+    if [ "$?" == "0" ]; then
+        # FILE has the tst pattern
+        if [ "$UPDATE" == "true" ]; then
+            # update line in FILE 
+            SED_COMMAND="s/$PATTERN_PATH/$LINE"
+            sed -i.bak -e "s|source.*tst.inc.*|$LINE|" $FILE
+            CHANGES_MADE="true"
+            print "File $FILE updated.\n" $IMPORTANT
+        else
+            # don't update
+            return 0
+        fi
+    else
+        # FILE doesn't have the tst pattern: add LINE
+        echo -e "\n# Next line configures environment for the TST CLI" >> $FILE
+        echo "$LINE" >> $FILE
+        print "Three lines added to $FILE.\n" $IMPORTANT
+        CHANGES_MADE="true"
+    fi
 }
 
 # MAIN
-
-if [ "$1" == "--non-interactive" ]; then
-    INTERACTIVE="false"
-else
-    INTERACTIVE="true"
+if [ "$1" == "--update" ]; then
+    UPDATE="true"
 fi
 
 # configure .profile
-CHANGES_MADE="false"
-if [ ! -f $USER_DOT_PROFILE ]; then
-    create_dot_profile
-    update_dot_profile
-    CHANGES_MADE="true"
-    print "File ~/.profile created.\n" $IMPORTANT
-else
-    # .profile DOES exist: check if it is configured
-    CHECK=$(grep -E "source.*tst.path.inc" $USER_DOT_PROFILE 2> /dev/null)
-    if [ "$?" == "0" ]; then
-        # .profile looks ok
-        if [ "INTERACTIVE" == "true" ]; then
-            print "$USER_DOT_PROFILE looks ok. Not modified.\n" $NORMAL
-        fi
-    else
-        # .profile does not look ok: create backup
-        print "* $USER_DOT_PROFILE" $NORMAL
-        print " => " $WARNING
-        print "$USER_DOT_PROFILE.bak\n" $NORMAL
-        cp $USER_DOT_PROFILE $USER_DOT_PROFILE.bak
-        update_dot_profile
-        CHANGES_MADE="true"
-        print "File ~/.profile updated.\n" $IMPORTANT
-    fi
+if [ -f $DOT_PROFILE ]; then
+    update_file $DOT_PROFILE
 fi
 
-# configure .bash_profile
-if [ ! -f $USER_DOT_BASH_PROFILE ]; then
-    create_dot_bash_profile
-    update_dot_bash_profile
-    CHANGES_MADE="true"
-    print "File ~/.bash_profile created.\n" $IMPORTANT
-else
-    CHECK=$(grep -E ".profile.*\. .*profile" $USER_DOT_BASH_PROFILE 2> /dev/null)
-    if [ "$?" == "0" ]; then
-        # .bash_profile looks ok
-        if [ "INTERACTIVE" == "true" ]; then
-            print "$USER_DOT_BASH_PROFILE looks ok. Not modified.\n" $NORMAL
-        fi
-    else
-        # .bash_profile does not look ok
-        print "* $USER_DOT_BASH_PROFILE" $NORMAL
-        print " => " $WARNING
-        print "$USER_DOT_BASH_PROFILE.bak\n" $NORMAL
-        cp $USER_DOT_BASH_PROFILE $USER_DOT_BASH_PROFILE.bak
-        update_dot_bash_profile
-        CHANGES_MADE="true"
-        print "File ~/.bash_profile updated.\n" $IMPORTANT
-    fi
+# create .bashrc, if it doesn't exist
+if [ ! -f $DOT_BASHRC ]; then
+    touch .bashrc
 fi
 
-if [ "$CHANGES_MADE" == "false" ]; then
-    print "Environment seems ok. No changes made.\n" $IMPORTANT
+update_file $DOT_BASHRC
+
+if [ "$CHANGES_MADE" != "true" ]; then
+    print "Environment seems ok. No changes made.\n"
 fi
