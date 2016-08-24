d1394deb137fba6a92b7985cc4dc7f08091d9874
Dalton Serey <daltonserey@gmail.com>
2016-08-24 15:35:24 -0300

    Add message to source .bashrc after install

--- etc/install.sh
+++ etc/install.sh
@@ -220,6 +220,10 @@ if [ "$INTERACTIVE" == "true" ]; then
         print "Remember to add $IMPORTANT$TST_DIR/bin $NORMAL to your PATH\n"
         exit
     fi
+    print "Remember that to use tst immediately, you must, either:" $IMPORTANT
+    print "- type the command:$IMPORTANT source ~/.bashrc"
+    print "  or"
+    print "- close the current shell and open a new one."
 fi
 
 # delete/rename previous installation
