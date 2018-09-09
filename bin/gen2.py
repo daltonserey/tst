e84ba15b475cd56733ff693b198da4b62c6f8add
Dalton Serey <daltonserey@gmail.com>
2018-09-09 17:06:36 -0300

    Improved version of tst-gentests

--- bin/gen2.py
+++ bin/gen2.py
@@ -1,73 +1,169 @@
 # coding: utf-8
+"""
+TST testcase generator.
+
+This module provides a single public function named create_test_suite
+that creates a test suite containing a set of testcases based on a
+given specification. The module does not provide any kind of user
+interface. Import the function directly as in:
+
+    from gen2 import create_test_suite
+
+Optionally, use the provided main function that imports the 
+specification from a file named specs.yaml and prints the test
+suite as a json in the output. Run as follows:
+
+    python -m gen2
+
+    or
+
+    python gen2.py
+"""
 import sys
+import os
 import yaml
-import random as random
 import json
+import codecs
+import random
 import hashlib
 
-from jinja2 import Environment, FileSystemLoader, Template
-from datagen import word, integer, reset_values
+import tstlib as tst
 
-# create jinja environment and read template
-env = Environment(loader=FileSystemLoader('.', encoding='utf-8'), keep_trailing_newline=True)
-template = env.get_template('template.yaml')
+from jinja2 import Template
+from datagen import DataGenerator
 
-# read tests specification list
-speclist = yaml.load(open("specs.yaml").read())
+dg = DataGenerator()
 
-include_all = False
+def create_test_suite(spec, include_all_tests):
+    # read test cases and parameters specification
+    cases_spec = spec['cases']
+    parameters = spec.get('parameters', [])
 
-# process each spec item
-tests = []
-for spec in speclist:
-    # read values and cases from spec
-    values = spec.get('values', [])
-    cases = spec['cases']
+    # read templates
     input_template = Template(spec['input_template'], keep_trailing_newline=True)
     output_template = Template(spec.get('output_template', ''), keep_trailing_newline=True)
 
-    covered_by_all_tests = set([])
-    while len(covered_by_all_tests) != len(cases):
+    test_suite, cases_covered_by_suite = [], set([])
+    while True:
+        if len(cases_covered_by_suite) == len(cases_spec): break
 
-        old_number = len(covered_by_all_tests)
-
-        # calculate test values
-        this_test_values = {}
-        for value in values:
-            val_name, val_spec = value.items()[0]
-            value = eval(str(val_spec))
-            globals()[val_name] = value
-            this_test_values[val_name] = value
-
-        this_test_values.update({
-            "integer": integer,
+        test_values = generate_values(parameters)
+        test_values.update({
+            "integer": dg.integer,
+            "word": dg.word,
             "max": max,
             "sum": sum
         })
+        cases_covered = cases_covered_by_test(test_values, cases_spec)
+        old_len_cases_covered_by_suite = len(cases_covered_by_suite)
+        cases_covered_by_suite.update(cases_covered)
 
-        # update coverage
-        covered_by_this_test = []
-        for case_name, case_spec in cases.items():
-            if eval(str(case_spec), this_test_values):
-                covered_by_this_test.append("%s" % case_name)
-                covered_by_all_tests.add(case_name)
+        if len(cases_covered_by_suite) > old_len_cases_covered_by_suite or include_all_tests:
+            testcase = create_testcase(test_values, cases_covered, input_template, output_template)
+            test_suite.append(testcase)
 
-        if len(covered_by_all_tests) > old_number or include_all:
-            input_text = input_template.render(this_test_values)
-            input_sha1 = hashlib.sha1(input_text).hexdigest()
-            tests.append({
-                "input": input_text,
-                "output": output_template.render(this_test_values),
-                "cases": covered_by_this_test,
-                "sha1": input_sha1
-            })
+        dg.reset_values()
 
-        reset_values()
+    return test_suite
 
-print json.dumps({
-    "tests": tests,
-    },
-    indent=2,
-    separators=(',', ': '),
-    ensure_ascii=False
-).encode('utf-8')
+
+def generate_values(parameters):
+    """Create test values from a give list of parameters, containing names and
+    specifications. Each parameter consists of a map with a single item. The
+    item key is taken as the value name and the item value is assumed to be the
+    value specification, written as a python expression.
+
+    Returns a map containing the value name mapped to the actual value
+    calculated by evaluating the specification.
+
+    CAUTION: eval() is used to evaluate each value spec.
+    """
+    values = {}
+    for value in parameters:
+        val_name, val_spec = value.items()[0]
+        value = eval(str(val_spec))
+        values[val_name] = value
+
+    return values
+
+
+def cases_covered_by_test(test_values, cases_spec):
+    """
+    Calculate the cases covered by a test case based on test_values.
+
+    CAUTION: eval() is used to evaluate each case spe.
+    """
+    cases_covered = []
+    for case_name, case_spec in cases_spec.items():
+        if eval(str(case_spec), test_values):
+            # … test_values validate this case condition
+            cases_covered.append(case_name)
+
+    return cases_covered
+
+
+def create_testcase(test_values, cases_covered, input_template, output_template):
+    """
+    Create an object (a map) containing the properties of a test case for tst.
+    It also includes the list of cases covered as well as the sha1 of the input
+    of test case. This can be considered as unique identifier of the test case.
+    """
+    input_text = input_template.render(test_values)
+    input_sha1 = hashlib.sha1(input_text).hexdigest()
+    
+    return {
+        "input": input_text,
+        "output": output_template.render(test_values),
+        "cases": cases_covered,
+        "sha1": input_sha1
+    }
+
+
+def main():
+    if not os.path.exists("specs.yaml"):
+        tst.cprint(tst.LRED, "no specs.yaml found")
+        sys.exit()
+
+    if os.path.exists("tst.json"):
+        with codecs.open("tst.json", mode='r', encoding='utf-8') as f:
+            tst_json = json.load(f)
+    else:
+        tst_json = {"tests": []}
+
+    # read specification
+    speclist = yaml.load(open("specs.yaml").read())
+
+    # add checksum to each existing test
+    tests = tst_json["tests"]
+    for test in tests:
+        test['sha1'] = test.get('sha1') or hashlib.sha1(test['input']).hexdigest()
+
+    # create tests for each specification element
+    for spec in speclist:
+        new_tests = create_test_suite(spec, include_all_tests=False)
+        tst.cprint(tst.LGREEN, "%d new tests created" % len(new_tests))
+        
+        # discard duplicated tests
+        new_checksums = {test['sha1'] for test in new_tests}
+        num_repeated = 0
+        for i in range(len(tests) - 1, -1, -1):
+            if tests[i]['sha1'] in new_checksums:
+                tests.pop(i)
+                num_repeated += 1
+
+        tests.extend(new_tests)
+
+    # write tests in tst.json file
+    with codecs.open("tst.json", mode='w', encoding='utf-8') as f:
+        f.write(json.dumps(tst_json,
+            indent=2,
+            separators=(',', ': '),
+            ensure_ascii=False
+        ).encode('utf-8'))
+
+    num_added = len(new_tests) - num_repeated
+    tst.cprint(tst.LGREEN, "%d tests added (%d discarded as repeated)" % (num_added, num_repeated))
+
+
+if __name__ == '__main__':
+    main()
