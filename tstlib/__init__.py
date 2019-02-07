e3a32e56c06dabf99cfbfd9def3468bec30eb832
Dalton Serey <daltonserey@gmail.com>
2019-02-07 10:08:53 -0300

    Move tstlib.pop_* functions to popargs submodule

--- tstlib/__init__.py
+++ tstlib/__init__.py
@@ -10,6 +10,7 @@ from __future__ import unicode_literals
 from config import Config
 from colors import *
 from utils import to_unicode
+from popargs import *
 
 import os
 import sys
@@ -74,45 +75,6 @@ def show(data):
     print(data2json(data).encode('utf-8'))
     sys.exit()
     
-
-def pop_argument(args, index=0):
-    if index >= len(args):
-        return None
-    
-    return args.pop(index)
-
-
-def pop_option(args, option, short=None, default=None, vtype=str):
-    if type(option) == list:
-        selectors = ['--' + opt for opt in option]
-    else:
-        selectors = ['--' + option]
-
-    if short is not None:
-        selectors.append('-' + short)
-
-    index = next((i for i in xrange(len(args)) if args[i] in selectors), None)
-    if index is None or index == len(args) - 1:
-        return default
-
-    value = args.pop(index + 1)
-    selector = args.pop(index)
-    
-    return vtype(value)
-
-
-def pop_flag(args, flag, short=None):
-    selectors = ['--' + flag]
-    if short is not None:
-        selectors.append('-' + short)
-
-    index = next((i for i in xrange(len(args)) if args[i] in selectors), None)
-    if index is None:
-        return False
-
-    args.pop(index)
-    return True
-
 
 class CorruptedFile(Exception): pass
 class ConnectionFail(Exception): pass
