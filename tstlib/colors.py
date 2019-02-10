2c1c14ec81d67fa7529880e896ba318611187b92
Dalton Serey <daltonserey@gmail.com>
2019-02-10 09:17:08 -0300

    Move cprint to colors.py

--- tstlib/colors.py
+++ tstlib/colors.py
@@ -1,3 +1,7 @@
+from __future__ import print_function
+
+import sys
+
 YELLOW = '\033[1;33m'
 LRED = '\033[1;31m'
 LGREEN = '\033[1;32m'
@@ -6,3 +10,5 @@ WHITE ="\033[1;37m"
 LCYAN = '\033[1;36m'
 RESET = '\033[0m'
 
+def cprint(color, msg, file=sys.stdout):
+    print(color + msg + RESET, file=file)
