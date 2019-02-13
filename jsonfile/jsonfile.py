f6f5832c03e72e4d53afcadb0b5dc9b0813f1cb3
Dalton Serey <daltonserey@gmail.com>
2019-02-13 17:29:37 -0300

    Add to_unicode back to jsonfile
    The module jsonfile is independent and is used by tst. Thus
    it should not depend on tst to work. Hence, the to_unicode
    function has been copied back to jsonfile.

--- jsonfile/jsonfile.py
+++ jsonfile/jsonfile.py
@@ -10,7 +10,19 @@ import sys
 import os
 import json
 
-from tst.utils import to_unicode
+def to_unicode(obj, encoding='utf-8'):
+    assert isinstance(obj, basestring), type(obj)
+    if isinstance(obj, unicode):
+        return obj
+
+    for encoding in ['utf-8', 'latin1']:
+        try:
+            obj = unicode(obj, encoding)
+            return obj
+        except UnicodeDecodeError:
+            pass
+
+    assert False, "tst: non-recognized encoding"
 
 class CorruptedJsonFile(Exception): pass
 
