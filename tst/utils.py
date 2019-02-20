9629ce5e70482b24410695988d1acbc652d9390f
Dalton Serey <daltonserey@gmail.com>
2019-02-20 14:47:18 -0300

    Add function is_posix_filename to module tst.utils

--- tst/utils.py
+++ tst/utils.py
@@ -1,8 +1,14 @@
 from __future__ import print_function
 
 import sys
+import string
 
 from colors import *
+
+
+def is_posix_filename(name, extra_chars=""):
+    CHARS = string.letters + string.digits + "._-" + extra_chars
+    return all(c in CHARS for c in name)
 
 
 def cprint(color, msg, file=sys.stdout, end='\n'):
