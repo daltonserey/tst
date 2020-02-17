ffdcedb5d40012559ac9186a60a9af8282d04ea6
Dalton Serey <daltonserey@gmail.com>
2020-02-17 17:31:39 -0300

    update other files to use correct str

--- tst/climenu/__init__.py
+++ tst/climenu/__init__.py
@@ -4,7 +4,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from builtins import input
+from builtins import input, str
 
 import sys
 import curses
--- tst/server.py
+++ tst/server.py
@@ -3,6 +3,8 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+from builtins import str
+
 import sys
 import signal
 import json
