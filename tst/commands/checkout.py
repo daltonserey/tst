2a44789f8929ec9edcb40fe3cc74822e96963d0b
Dalton Serey <daltonserey@gmail.com>
2022-02-21 19:36:39 -0300

    remove server interaction commands
    The login, checkout and commit commands were moved to the p1ufcg
    package, as they were used to interact with the p1 server.

--- tst/commands/checkout.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-from tst.colors import *
-from tst.utils import cprint
-
-def main():
-    cprint(LRED, "tst: checkout command is deprecated")
-    sys.exit(1)
--- tst/commands/login.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-from tst.colors import *
-from tst.utils import cprint
-
-def main():
-    cprint(LRED, "tst: login command is deprecated")
-    sys.exit(1)
