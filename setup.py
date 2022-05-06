daltonserey/tst

commit 6158dba522f53ac6ef598241b75bf435597daa8b

fix: json report not printed if file doesnt exist
First, the json report didn't print a json output when a single
filename is given in command line and the file doesn't exist. If
more than one file is given, however, the json output is
correctly printed. Second, the summary for the error (when the
file doesn't exist) is more specific.

tst/commands/test.py
@@ -51,6 +51,7 @@ STATUS_CODE = {
     'ValueError': 'v',
     'TypeError': 'y',
     'NameError': 'n',
+    '[Errno 2]': '?'
 }
 
 def alarm_handler(_1, _2):
@@ -690,7 +691,7 @@ def parse_cli():
     args = parser.parse_args()
 
     # identify answer files (files to be tested)
-    if len(args.filename) == 1 and os.path.exists(args.filename[0]):
+    if len(args.filename) == 1: # and os.path.exists(args.filename[0]):
         files2test = [args.filename[0]]
     elif len(args.filename) == 1:
         fn_pattern = '*%s*' % args.filename[0]
@@ -785,7 +786,7 @@ def main():
 
     reporter = Reporter.get(style=style, options=options)
     reporter.num_tests = len(subjects) * number_of_tests
-    count, script_errors = 0, []
+    count = 0
     for subject in subjects:
         for ts in test_suites:
             testcases = ts[1]
@@ -794,7 +795,6 @@ def main():
                 count += 1
                 testrun = TestRun(subject, testcase, index=count)
                 testresult = testrun.run(timeout=options['timeout'])
-                ('error' in testresult) and script_errors.append(testresult)
                 subject.add_testrun(testrun)
                 reporter.update(subject, testcase, testresult)
             log.removeHandler(handler_console)

commit 86a1f781914a5508b2dc516d1f51c7524564dea7

fix: fix 6158dba introduced a new bug

setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tst',
-    version="0.15.3",
+    version="0.15.4",
     description='TST Simple Test Runner',
     url='http://github.com/daltonserey/tst',
     author='Dalton Serey',

tst/commands/test.py
@@ -691,7 +691,7 @@ def parse_cli():
     args = parser.parse_args()
 
     # identify answer files (files to be tested)
-    if len(args.filename) == 1: # and os.path.exists(args.filename[0]):
+    if len(args.filename) == 1 and os.path.exists(args.filename[0]):
         files2test = [args.filename[0]]
     elif len(args.filename) == 1:
         fn_pattern = '*%s*' % args.filename[0]
@@ -772,6 +772,15 @@ def main():
     ignore_files = tstjson.get('ignore', []) + config.get('ignore', [])
     files2test = [f for f in files2test if any(f.endswith(e) for e in extensions) and f not in ignore_files]
     files2test.sort()
+
+    # identify style
+    style = options['output-format'] or tst.get_config().get('output-format')
+    style = "debug" if options['diff'] or options['compare'] else style
+    if style == "json" and not files2test:
+        # TODO: fix this special case
+        print({})
+        sys.exit(1)
+        
     _assert(files2test, 'No files to test')
     options["max_fn_width"] = max(len(fn) for fn in files2test)
 
@@ -779,10 +788,6 @@ def main():
     subjects = [TestSubject(fn) for fn in files2test]
 
     _assert(not (options["diff"] or options["compare"]) or len(subjects) == 1, "option --diff/--compare cannot be used with multiple subjects")
-
-    # identify style
-    style = options['output-format'] or tst.get_config().get('output-format')
-    style = "debug" if options['diff'] or options['compare'] else style
 
     reporter = Reporter.get(style=style, options=options)
     reporter.num_tests = len(subjects) * number_of_tests