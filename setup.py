095710dd31d18bd7c27cbba7b625bb5b5f98f0f8
Dalton Serey <daltonserey@gmail.com>
2022-05-20 11:47:34 -0300

    update to version 0.17.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tst',
-    version="0.17.0",
+    version="0.17.1",
     description='TST Simple Test Runner',
     url='http://github.com/daltonserey/tst',
     author='Dalton Serey',
